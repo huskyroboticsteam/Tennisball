56b282b94e6228ac5c5a746c7f838e1916e3bb2b
Evan <evan.straw99@gmail.com>
2018-10-30 21:12:31 -0700

    Add code for filtering the images coming through the camera

--- live_video.cpp
+++ live_video.cpp
@@ -1,10 +1,67 @@
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 using namespace cv;
 using namespace std;
+
+void show(Mat image){
+  imshow("Live (Original)",image);
+}
+
+void showGray(Mat image){
+  Mat gray_image;
+  cvtColor(image, gray_image, COLOR_RGB2GRAY);
+  imshow("Live (Grayscale)",gray_image);
+}
+
+void showBGR(Mat image){
+  Mat bgr_image;
+  cvtColor(image, bgr_image, COLOR_RGB2BGR);
+  imshow("Live (BGR)",bgr_image);
+}
+
+Mat morphed_img(Mat mask)
+{
+  Mat se21 = getStructuringElement(MORPH_RECT, Size(21, 21));
+  Mat se11 = getStructuringElement(MORPH_RECT, Size(11, 11));
+
+  morphologyEx(mask, mask, MORPH_CLOSE, se21);
+  morphologyEx(mask, mask, MORPH_OPEN, se11);
+
+  GaussianBlur(mask, mask, Size(15, 15), 0, 0);
+  return(mask);   
+}
+
+Mat threshold_image(Mat img)
+{
+  Mat hsv(img.rows, img.cols, CV_8UC3);
+  cvtColor(img, hsv, CV_RGB2HSV);
+  Mat thresh(img.rows, img.cols, CV_8UC1);
+  Scalar high_HSV(127,64,16);
+  Scalar low_HSV(146,255,255);
+  inRange(hsv, high_HSV, low_HSV, thresh);
+  return(thresh);
+}
+
+void showFiltered(Mat image, bool morph){
+  Mat mask = threshold_image(image);
+  if(morph){
+  Mat filtered = morphed_img(mask);
+  imshow("Live (Filtered)",filtered);
+  }
+  else{
+    imshow("Live (Filtered)",mask);
+  }
+}
+
+int lowH, lowS, lowV;
+int highH, highS, highV;
+
+
+
 int main(int, char**)
 {
     Mat frame;
@@ -25,7 +82,7 @@ int main(int, char**)
     //--- GRAB AND WRITE LOOP
     cout << "Start grabbing" << endl
         << "Press any key to terminate" << endl;
-    for (;;)
+    while(true)
     {
         // wait for a new frame from camera and store it into 'frame'
         cap.read(frame);
@@ -34,8 +91,15 @@ int main(int, char**)
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
-        // show live and wait for a key with timeout long enough to show images
-        imshow("Live", frame);
+
+        show(frame);
+
+        //showGray(frame);
+
+        //showBGR(frame);
+
+        showFiltered(frame,false);
+
         if (waitKey(5) >= 0)
             break;
     }
