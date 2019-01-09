a269d27c49f1c829bb5ba6382a33da3dd45c32f9
Evan <evan.straw99@gmail.com>
2019-01-08 19:16:47 -0800

    Organization stuff

---
+++ install_models.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dir=`pwd`
+cd /tmp
+git clone https://github.com/tensorflow/models
+cd $dir
+rsync -ravP /tmp/models/research/object_detection ./
+protoc object_detection/protos/*.proto --python_out=.
+
