b3fe4ba98d651dc43e3200cef076acb542d08f16
Evan <evan.straw99@gmail.com>
2019-01-17 11:50:48 -0800

    Finalize training configuration
    Training actually works now!
    NOTE: The model_main.py in scripts/ is a modified version of the one provided by
    the object detection API models. It mainly just contains changes to the
    parameters that control number of steps to log and save after. It is
    automatically copied into the correct place by the install_models.sh script.
    
    Also, a slightly large change to make training work was to switch to Python 2
    instead of 3 because the Object Detection API doesn't like 3 apparently.

--- scripts/install_models.sh
+++ scripts/install_models.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 dir=`pwd`
+scripts_dir=$dir"/scripts"
 cd /tmp
+#clone the object detection models
 git clone https://github.com/tensorflow/models
 cd $dir
+#copy the object detection models into working dir
 rsync -ravP /tmp/models/research/object_detection ./
+#compile protobufs
 protoc object_detection/protos/*.proto --python_out=./object_detection/
-
+#copy the modified model_main into object_detection dir
+cp $scripts_dir/model_main.py ./object_detection/
--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -3,7 +3,8 @@ PIPELINE_CONFIG_PATH=./training/pipeline.config
 MODEL_DIR=./data/models/
 NUM_TRAIN_STEPS=50000
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python3 object_detection/model_main.py \
+mkdir -p $MODEL_DIR
+python object_detection/model_main.py \
        --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
        --model_dir=${MODEL_DIR} \
        --num_train_steps=${NUM_TRAIN_STEPS} \
