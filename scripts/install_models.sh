8c9dc763a5f9b3bcc8de54d3e3ceb87132f04253
Evan <evan.straw99@gmail.com>
2019-01-16 20:45:08 -0800

    Complete datasets.
    Added a bunch of images, and moved some files around.

--- scripts/install_models.sh
+++ scripts/install_models.sh
@@ -4,5 +4,5 @@ cd /tmp
 git clone https://github.com/tensorflow/models
 cd $dir
 rsync -ravP /tmp/models/research/object_detection ./
-protoc object_detection/protos/*.proto --python_out=.
+protoc object_detection/protos/*.proto --python_out=./object_detection/
 
