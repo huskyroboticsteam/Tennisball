29e6868d0ec53f271b9b554300d1b7a16905f35f
Evan <evan.straw99@gmail.com>
2019-01-29 19:59:41 -0800

    Extract detection functionality into header files
    Detection is now done with an instance of the Detector class, found in
    detector.cpp/hpp, and returns a vector of Detection objects, which represent a
    bounding box and a confidence value.

--- src/still_image.cpp
+++ src/still_image.cpp
@@ -4,57 +4,31 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
+#include "detector.hpp"
 
 using namespace std;
 using namespace cv;
 
 int main() {
-    string graphPath = "./data/final_models/frozen_inference_graph.pb";
-    string graphPbtxt = "./data/final_models/graph.pbtxt";
-    cv::dnn::Net network = cv::dnn::readNetFromTensorflow(graphPath, graphPbtxt);
+    std::string graphPath = "./data/final_models/frozen_inference_graph.pb";
+    std::string graphPbtxt = "./data/final_models/graph.pbtxt";
 
-    string classNames[] = { "", "TennisBall" };
+    tb::Detector det (graphPath, graphPbtxt);
 
     string imagePath = "./images/1.jpg";
     Mat frame = imread(imagePath);
 
-    network.setInput(cv::dnn::blobFromImage(frame, 1.0, Size(300, 300), Scalar(), true, false));
-    Mat output = network.forward();
-
-    cout << output.size << endl;
-    Mat detectionMat(output.size[2], output.size[3], CV_32F, output.ptr<float>());
-
-    for(int i = 0; i < detectionMat.rows; i++) {
-        float confidence = detectionMat.at<float>(i, 2);
-        cout << "confidence: " << confidence << endl;
-
-        if(confidence > 0.2) {
-            int objectClass = static_cast<int>(detectionMat.at<float>(i, 1));
-            cout << "object class: " << objectClass << endl;
-
-            int left = static_cast<int>(detectionMat.at<float>(i, 3) * frame.cols);
-            int bottom = static_cast<int>(detectionMat.at<float>(i, 4) * frame.rows);
-            int right = static_cast<int>(detectionMat.at<float>(i, 5) * frame.cols);
-            int top = static_cast<int>(detectionMat.at<float>(i, 6) * frame.rows);
-
-            cout << "[" << left << ", " << right << "] x [" << bottom << ", " << top << "]" << endl;
-
-            ostringstream ss;
-            ss << confidence * 100 << "%";
-            string conf(ss.str());
-
-            Rect object((int)left, (int)bottom, (int)(right - left), (int)(top - bottom));
-
-            rectangle(frame, object, Scalar(0, 255, 0), 2);
-            String label = classNames[objectClass] + ": " + conf;
-            int baseLine = 0;
-            Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-            rectangle(frame,
-                      Rect(Point(left, bottom - labelSize.height),
-                           Size(labelSize.width, labelSize.height + baseLine)),
-                      Scalar(0, 255, 0), CV_FILLED);
-            putText(frame, label, Point(left, bottom), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
-        }
+    vector<tb::Detection> detections = det.performDetection(frame);
+    int i = 0;
+    for(tb::Detection current : detections){
+        rectangle(frame, current.getBBoxRect(), Scalar(0, 255, 0), 2);
+        ostringstream output;
+        output << i << " - ";
+        output << "bbox: [" << current.getBBoxLeft() << ", " << current.getBBoxRight() << "]";
+        output << " x [" << current.getBBoxTop() << ", " << current.getBBoxBottom() << "] ";
+        output << "conf: " << current.getConfidencePct() << "%";
+        cout << output.str() << endl;
+        i++;
     }
     imshow("image", frame);
     waitKey(0);
